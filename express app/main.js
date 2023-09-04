6d4f54aa65af56de22f27617a2165cc27a61ca4e
ReemAbdelbary <101728693+ReemAbdelbary@users.noreply.github.com>
2023-09-04 20:12:16 +0300

    main.js

--- express app/main.js
+++ express app/main.js
@@ -7,26 +7,60 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 
 
-app.get('/',(req,res)=>{
+app.get('/items',(req,res)=>{
     res.json(todos);
 });
 
-app.post('/',(req,res)=>{
+// get selected item
+app.get('/items/:id',(req,res)=>{
+    var id = req.params.id ;
+    const item = todos.find((item)=> item.id === Number(id));
+    if(item)
+    res.json(item);
+    else
+    res.json({ message : "item doesn't exist"});
+});
+
+app.post('/items',(req,res)=>{
     const body = req.body;
     todos.push(body);
     res.json({ message : "item added to the list"});
 });
 
-app.delete('/',(req,res)=>{
+app.delete('/items',(req,res)=>{
     todos = [];
     res.json({ message : "todo list is deleted"})
 });
 
-app.put('/',(req,res)=>{
+//delete selected item
+app.delete('/items/:id',(req,res)=>{
+    var id = req.params.id ;
+    todos = todos.filter((item)=> item.id != Number(id));
+    res.json({ message : "item is deleted"})
+});
+
+app.put('/items',(req,res)=>{
     todos = [];
     const body = req.body;
     todos.push(body);
     res.json({ message : "todo list is updated"});
+});
+
+//update selected item
+app.put('/items/:id',(req,res)=>{
+    var id = Number(req.params.id) ;
+    const body = req.body;
+    var flag = 0;
+    for(var i=0 ; i<todos.length ; i++){
+        if(todos[i].id === id){
+            todos[i]=body;
+            flag =1 ;
+        }
+    }
+    if(flag)
+    res.json({ message : "todo list is updated"});
+    else
+    res.json({ message : "item doesn't exist to be updated"});
 });
 
 app.listen(3000,'localhost',()=>{
